089c22e2461f3d250f1ebbd23f5b42e98d0cbbb7
Niel-nts <nt.s@live.com>
2023-09-15 19:38:48 -0300

    adicionado midlewares e tratamentos de erros

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,6 +4,13 @@ const UsersController = require("../controllers/UsersController")
 const usersRoutes = Router()
 const usersController = new UsersController()
 
-usersRoutes.post("/", usersController.create)
+function myMiddleware(request, response, next){
+    console.log('Você passou pelo middleware')
+    if(!request.body.isAdmin){
+        return response.json({message: "user unauthorized"})
+    }
+}
+
+usersRoutes.post("/", myMiddleware, usersController.create)
 
 module.exports = usersRoutes
--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+require("express-async-errors")
+
+const AppError = require("./utils/AppError")
+
 const express = require("express");
 const Routes = require("./routes")
 
