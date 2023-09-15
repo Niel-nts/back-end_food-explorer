cd6542c7ec12efbf3794e841582ee705b1d0eebe
Niel-nts <nt.s@live.com>
2023-09-15 20:28:36 -0300

    adicionado o sqlite

--- src/server.js
+++ src/server.js
@@ -1,14 +1,33 @@
 require("express-async-errors")
 
+const database = require("./database/sqlite")
 const AppError = require("./utils/AppError")
 
 const express = require("express");
-const Routes = require("./routes")
+const routes = require("./routes")
 
 const app = express();
 app.use(express.json());
 
-app.use("/", Routes)
+app.use(routes)
+
+database()
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status:"error",
+            message: error.message
+        })
+    }
+
+    console.error(error)
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+})
 
 const PORT = 3333;
 
