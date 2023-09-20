3433c2ec4877c8ddafeab0cbedacccf1b9a595e0
Niel-nts <nt.s@live.com>
2023-09-19 23:15:34 -0300

    adicionado automatizações para criação de tabelas

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,11 +1,13 @@
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 const sqliteConnection = require("../database/sqlite")
 class UsersController {
     async create(request, response){
         const { name, email, password } = request.body;
 
-        const database = await sqliteConnection()     
+        const database = await sqliteConnection()
+        
+        const user = await database.get("select * from users where id = (?)", [id])
         
         const checkUserExists = await database.get('select * from users where email = (?)', [email])
 
@@ -21,7 +23,7 @@ class UsersController {
     }
 
     async update (request, response){
-        const {name, email} = request.body
+        const {name, email, password, old_password} = request.body
         const {id} = request.params
 
         const database = await sqliteConnection()
@@ -37,8 +39,22 @@ class UsersController {
             throw new AppError("Este email está em uso")
         }
 
-        user.name = name
-        user.email = email
+        user.name = name ?? user.name
+        user.email = email ?? user.email
+
+        if(password && !old_password){
+            throw new AppError("Você precisa digitar a senha antiga")
+        }
+
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password)
+
+            if(!checkOldPassword){
+                throw new AppError("A senha antiga não confere")
+            }
+
+            user.password = await hash(password, 8)
+        }
 
         await database.run(`
             updata users set 
